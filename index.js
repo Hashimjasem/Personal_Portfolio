55d919c0afadabc56d48a11d96483484af38307a
Hashimjasem <j_hashim1305@outlook.com>
2024-01-04 17:28:35 +0800

    fonts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //elements
-// nav buttons
-logobtn = document.getElementById("logo-btn")
-menuBtn = document.getElementById("menu")
-lightdark = document.getElementById("lightdark")
-// landing buttons
-toProjBtn = document.getElementById("to-proj")
-toAboutBtn = document.getElementById("to-about")
-
+// // nav buttons
+// logobtn = document.getElementById("logo-btn")
+// menuBtn = document.getElementById("menu")
+// lightdark = document.getElementById("lightdark")
+// // landing buttons
+// toProjBtn = document.getElementById("to-proj")
+// toAboutBtn = document.getElementById("to-about")
+import Swup from 'swup';
+const swup = new Swup();
