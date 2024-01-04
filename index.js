ebdf3c8d44d55799a1f808ec24296b5ecffc50d3
Hashimjasem <j_hashim1305@outlook.com>
2024-01-04 22:10:17 +0800

    animations for landing, about & menu

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-//elements
-// // nav buttons
-// logobtn = document.getElementById("logo-btn")
-// menuBtn = document.getElementById("menu")
-// lightdark = document.getElementById("lightdark")
-// // landing buttons
-// toProjBtn = document.getElementById("to-proj")
-// toAboutBtn = document.getElementById("to-about")
-import Swup from 'swup';
-const swup = new Swup();
+removeLoad = (loadingscn) =>{
+    loadingscn = document.getElementById('loading-screen')
+    loadingscn.remove();
+}
+
+setTimeout(removeLoad,1500)
