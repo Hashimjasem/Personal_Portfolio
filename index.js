a883a885c448e74a73049e6619a7a6e93a91bc2d
Hashimjasem <j_hashim1305@outlook.com>
2024-01-19 16:10:40 +0800

    images for project section

--- index.js
+++ index.js
@@ -9,73 +9,69 @@ let projectsGrid = document.querySelector('#projects-grid')
 
 let projects = [
     {
-        name: 'my24',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
-        name: 'python API',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
-        name: 'javascript API',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
-        name: 'Twelver',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
-        name: 'Najaf Mens Health Clinic',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
-        name: 'Java API',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
-        name: 'KOOTA Mobile Detailers',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
-        name: 'Bun API',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
         name: 'NPM Animation Library',
-        img: './assets/IMG_2493.JPG',
-    },
-    {
-        name: 'Online Games',
-        img: './assets/IMG_2493.JPG',
+        img: './assets/npm.png',
     },
     {
         name: 'Snake',
-        img: './assets/IMG_2493.JPG',
+        img: './assets/Snake_OG-logo.png',
+    },
+    {
+        name: 'Bun API',
+        img: './assets/BUN.png',
+    },
+    {
+        name: 'Personal Portfolio',
+        img: './assets/hashimjasem.jpeg',
+    },
+    {
+        name: 'Twelver',
+        img: './assets/twelver.png',
     },
     {
         name: 'Minesweeper',
-        img: './assets/IMG_2493.JPG',
+        img: './assets/Minesweeper.png',
+    },
+    {
+        name: 'KOOTA Mobile Detailers',
+        img: './assets/KOOTA.png',
+    },
+    {
+        name: 'Najaf Mens Health Clinic',
+        img: './assets/najafLogo.png',
+    },
+    {
+        name: 'nodejs API',
+        img: './assets/nodejs.png',
+    },
+    {
+        name: 'Online Games',
+        img: './assets/friv.png',
+    },
+    {
+        name: 'my24',
+        img: 'assets/my24.png',
     }
 ]
 // this array represents the slots in the bento box. eg 24 represents a 2 by 4 slot in the grid
-bentoSlots = ['tf', 'oo','oo','tt','tt','ot','tt','oo','to','ot','oe']
+bentoSlots = ['tf', 'oo', 'oo', 'tt', 'tt', 'ot', 'tt', 'oo', 'to', 'ot', 'oe']
 
 // Shuffle the projects array randomly
 projects.sort(() => 0.5 - Math.random());
 
 bentoSlots.forEach(slot => {
-  const project = projects.pop(); // Get a random project from the end of the array
-  const projectElement = document.createElement('btn');
-  projectElement.classList.add('project-item');
-  projectElement.style.backgroundImage = `url(${project.img})`;
-  projectElement.innerHTML = `
+    const project = projects.pop(); // Get a random project from the end of the array
+    const projectElement = document.createElement('btn');
+    projectElement.classList.add('project-item');
+    projectElement.style.backgroundImage = `url(${project.img})`;
+    projectElement.innerHTML = `
     <div class="project-card">
     ${project.name}
     </div>
   `;
-  // Determine the class based on the slot number
-  const [rows, columns] = slot.toString().split('');
-  projectElement.classList.add(`${rows}-by-${columns}`);
+    // Determine the class based on the slot number
+    const [rows, columns] = slot.toString().split('');
+    projectElement.classList.add(`${rows}-by-${columns}`);
 
-  projectsGrid.appendChild(projectElement);
+    projectsGrid.appendChild(projectElement);
 });
