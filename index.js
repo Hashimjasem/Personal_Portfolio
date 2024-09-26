c0ec966a2cdeb3ecd86323d26997e135430e531d
Hashimjasem <j_hashim1305@outlook.com>
2024-09-26 17:29:36 +0800

    styling changes

--- index.js
+++ index.js
@@ -1,73 +1,75 @@
-let tech = [
-    {
-        name: "HTML",
-        bg: "./assets/html.png"
-    },
-    {
-        name: "CSS",
-        bg: "./assets/css.png"
-    },
-    {
-        name: "JS",
-        bg: "./assets/js.png"
-    },
-    {
-        name: "ReactJS",
-        bg: "./assets/react.png"
-    },
-    {
-        name: "NodeJS",
-        bg: "./assets/nodejs.png"
-    },
-    {
-        name: "ExpressJS",
-        bg: "./assets/expressjs.png"
-    },
-    {
-        name: "NextJS",
-        bg: "./assets/nextjs.png"
-    },
-    {
-        name: "MongoDB",
-        bg: "./assets/mongodb.png"
-    },
-    {
-        name: "MySQL",
-        bg: "./assets/mysql.png"
-    },
-    {
-        name: "GSAP",
-        bg: "./assets/Gsap.png"
-    },
-    {
-        name: "Tailwind",
-        bg: "./assets/tailwind.png"
-    },
-    {
-        name: "Figma",
-        bg: "./assets/figma.png"
-    },
-    // Add more objects as needed
-];
+// let tech = [
+//     {
+//         name: "HTML",
+//         bg: "./assets/html.png"
+//     },
+//     {
+//         name: "CSS",
+//         bg: "./assets/css.png"
+//     },
+//     {
+//         name: "JS",
+//         bg: "./assets/js.png"
+//     },
+//     {
+//         name: "ReactJS",
+//         bg: "./assets/react.png"
+//     },
+//     {
+//         name: "NodeJS",
+//         bg: "./assets/nodejs.png"
+//     },
+//     {
+//         name: "ExpressJS",
+//         bg: "./assets/expressjs.png"
+//     },
+//     {
+//         name: "NextJS",
+//         bg: "./assets/nextjs.png"
+//     },
+//     {
+//         name: "MongoDB",
+//         bg: "./assets/mongodb.png"
+//     },
+//     {
+//         name: "MySQL",
+//         bg: "./assets/mysql.png"
+//     },
+//     {
+//         name: "GSAP",
+//         bg: "./assets/Gsap.png"
+//     },
+//     {
+//         name: "Tailwind",
+//         bg: "./assets/tailwind.png"
+//     },
+//     {
+//         name: "Figma",
+//         bg: "./assets/figma.png"
+//     },
+//     // Add more objects as needed
+// ];
 
-let projects
-
-
-let technofill = (tech) => {
-    for (let i = 0; i < 2; i++) {
-        // Create a <ul> element
-        let ul = document.getElementById("technologies-list");
-
-        // Iterate through the array of objects
-        tech.forEach(item => {
-            // Create a <li> element for each item
-            let li = document.createElement("li");
-
-            li.style.backgroundImage = `url(${item.bg})`;
-            ul.appendChild(li);
-        });
-    }
-}
+// let projects
 
 
-technofill(tech)
+// let technofill = (tech) => {
+//     for (let i = 0; i < 2; i++) {
+//         // Create a <ul> element
+//         let ul = document.getElementById("technologies-list");
+
+//         // Iterate through the array of objects
+//         tech.forEach(item => {
+//             // Create a <li> element for each item
+//             let li = document.createElement("li");
+
+//             li.style.backgroundImage = `url(${item.bg})`;
+//             ul.appendChild(li);
+//         });
+//     }
+// }
+
+
+// technofill(tech)
+
+
